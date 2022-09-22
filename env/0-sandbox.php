f024a3fb35025b2be72edb2c80116e735d0e8e14
Kelly Dwan <ryelle@users.noreply.github.com>
2022-09-22 10:48:58 -0400

    Initial commit

---
+++ env/0-sandbox.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * These are stubs for closed source code, or things that only apply to local environments.
+ */
+
+defined( 'WPINC' ) || die();
+
+require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/loader.php';
---
+++ env/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+root=$( dirname $( wp config path ) )
+
+wp theme activate wporg-themes-2022
+
+wp rewrite structure '/%postname%/'
+wp rewrite flush --hard
+
+wp option update blogname "Theme Directory"
+wp option update blogdescription "Free WordPress Themes"
+
+wp import "{$root}/env/themes-pages.xml" --authors=create
+wp import "{$root}/env/themes-repopackages.xml" --authors=create
+wp import "{$root}/env/themes-theme_shops.xml" --authors=create
+
+wp db import "${root}/env/tables.sql"
