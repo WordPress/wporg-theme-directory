ff32f20f080d6c247ec450c863cfa94420aac6fb
Kelly Dwan <ryelle@users.noreply.github.com>
2024-04-26 14:46:15 -0400

    Add header and pagination controls to style variations

--- source/wp-content/themes/wporg-themes-2024/src/theme-style-variations/render.php
+++ source/wp-content/themes/wporg-themes-2024/src/theme-style-variations/render.php
@@ -10,14 +10,67 @@ if ( ! $current_post_id ) {
 
 $theme_post = get_post( $block->context['postId'] );
 $styles = get_theme_style_variations( $theme_post->post_name );
+$count = count( $styles );
 
-if ( ! count( $styles ) ) {
+if ( ! $count ) {
 	return '';
 }
 
+$label = sprintf(
+	/* translators: Heading for style variations, %s is the number of styles. */
+	_n( 'Style variations (%s)', 'Style variations (%s)', $count, 'wporg-themes' ),
+	$count
+);
+
+// Initial state to pass to Interactivity API.
+$init_state = [
+	'isRTL' => is_rtl(),
+];
+$encoded_state = wp_json_encode( $init_state );
+
 ?>
-<div <?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>>
-	<div class="wporg-theme-style-variations__row">
+<div
+	<?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>
+	data-wp-interactive="wporg/themes/style-variations"
+	data-wp-context="<?php echo esc_attr( $encoded_state ); ?>"
+	data-wp-init="actions.init"
+	data-wp-on-window--resize="actions.init"
+>
+	<div class="wporg-theme-style-variations__controls">
+		<h2><?php echo esc_html( $label ); ?></h2>
+		<div
+			class="wporg-theme-style-variations__buttons wp-block-buttons"
+			data-wp-bind--hidden="!state.hasOverscroll"
+		>
+			<div class="wp-block-button is-style-toggle is-small">
+				<button
+					tabindex="-1"
+					class="wp-block-button__link wp-element-button"
+					aria-label="Prev"
+					data-wp-on--click="actions.handlePrevious"
+					data-wp-bind--disabled="!state.canPrevious"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="M20 11.2H6.8l3.7-3.7-1-1L3.9 12l5.6 5.5 1-1-3.7-3.7H20z"></path></svg>
+				</button>
+			</div>
+			<div class="wp-block-button is-style-toggle is-small">
+				<button
+					tabindex="-1"
+					class="wp-block-button__link wp-element-button"
+					aria-label="Next"
+					data-wp-on--click="actions.handleNext"
+					data-wp-bind--disabled="!state.canNext"
+				>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path d="m14.5 6.5-1 1 3.7 3.7H4v1.6h13.2l-3.7 3.7 1 1 5.6-5.5z"></path></svg>
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="wporg-theme-style-variations__row"
+		data-wp-on--scroll="actions.handleScroll"
+	>
 		<?php
 		foreach ( $styles as $i => $style ) {
 			$style->preview_base = untrailingslashit( get_permalink( $theme_post ) ) . '/preview/';
---
+++ source/wp-content/themes/wporg-themes-2024/src/theme-style-variations/view.js
@@ -0,0 +1,78 @@
+/**
+ * WordPress dependencies
+ */
+import { getContext, getElement, store } from '@wordpress/interactivity';
+
+const OFFSET_WIDTH = 220;
+
+/**
+ * Get the "row" element starting with any block child element.
+ *
+ * @param {Element} ref
+ *
+ * @return {Element}
+ */
+function getRowElement( ref ) {
+	const container = ref.closest( '.wp-block-wporg-theme-style-variations' );
+	const element = container.querySelector( '.wporg-theme-style-variations__row' );
+	return element;
+}
+
+// Note about RTL: Browsers scroll "left" into the negative when on RTL, so the
+// logic for when we "canNext"/"canPrevious" are swapped, as are the offsets
+// triggered when clicking the arrow buttons.
+
+const { state } = store( 'wporg/themes/style-variations', {
+	state: {
+		get canPrevious() {
+			const { isRTL } = getContext();
+			if ( isRTL ) {
+				return state.position < 0;
+			}
+			return state.position > 0;
+		},
+		get canNext() {
+			const { isRTL } = getContext();
+			if ( isRTL ) {
+				return state.position >= state.overflow * -1;
+			}
+			return state.position < state.overflow;
+		},
+		get hasOverscroll() {
+			return state.canNext || state.canPrevious;
+		},
+		position: 0,
+		overflow: 0,
+	},
+	actions: {
+		init() {
+			const element = getRowElement( getElement().ref );
+			state.position = element.scrollLeft;
+			// How much extra scroll overflow do we have?
+			state.overflow =
+				element.children.length * 100 + ( element.children.length - 1 ) * 10 - element.clientWidth;
+		},
+		handleScroll() {
+			const element = getRowElement( getElement().ref );
+			state.position = element.scrollLeft;
+		},
+		handlePrevious() {
+			const { isRTL } = getContext();
+			const element = getRowElement( getElement().ref );
+			const position = isRTL ? element.scrollLeft + OFFSET_WIDTH : element.scrollLeft - OFFSET_WIDTH;
+			element.scrollTo( {
+				left: position,
+				behavior: 'smooth',
+			} );
+		},
+		handleNext() {
+			const { isRTL } = getContext();
+			const element = getRowElement( getElement().ref );
+			const position = isRTL ? element.scrollLeft - OFFSET_WIDTH : element.scrollLeft + OFFSET_WIDTH;
+			element.scrollTo( {
+				left: position,
+				behavior: 'smooth',
+			} );
+		},
+	},
+} );
