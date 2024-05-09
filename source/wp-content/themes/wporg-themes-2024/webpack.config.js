7b416bc1c980aabef37781d7799af0a1e1e9fd9d
Kelly Dwan <ryelle@users.noreply.github.com>
2024-05-09 15:06:14 -0400

    Explain why two configs

--- source/wp-content/themes/wporg-themes-2024/webpack.config.js
+++ source/wp-content/themes/wporg-themes-2024/webpack.config.js
@@ -1,7 +1,8 @@
 const RtlCssPlugin = require( 'rtlcss-webpack-plugin' );
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-// When using `--experimental-modules`, the config comes as two objects.
+// When using `--experimental-modules`, the config comes as two objects,
+// one for plain scripts and one for module scripts.
 // Add the RTL config to both.
 if ( Array.isArray( defaultConfig ) ) {
 	module.exports = defaultConfig.map( ( config ) => ( {
