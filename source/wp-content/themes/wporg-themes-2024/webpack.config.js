d13cfbb761c162f5c9cca62bb675e3d96dbb1f37
Kelly Dwan <ryelle@users.noreply.github.com>
2024-05-09 14:53:49 -0400

    Tools: Add RTLCSS step to theme build process

---
+++ source/wp-content/themes/wporg-themes-2024/webpack.config.js
@@ -0,0 +1,26 @@
+const RtlCssPlugin = require( 'rtlcss-webpack-plugin' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+// When using `--experimental-modules`, the config comes as two objects.
+// Add the RTL config to both.
+if ( Array.isArray( defaultConfig ) ) {
+	module.exports = defaultConfig.map( ( config ) => ( {
+		...config,
+		plugins: [
+			...config.plugins,
+			new RtlCssPlugin( {
+				filename: `[name]-rtl.css`,
+			} ),
+		],
+	} ) );
+} else {
+	module.exports = {
+		...defaultConfig,
+		plugins: [
+			...defaultConfig.plugins,
+			new RtlCssPlugin( {
+				filename: `[name]-rtl.css`,
+			} ),
+		],
+	};
+}
