052453d9620b8deef9643efef9ab320f909b9e6a
Kelly Dwan <ryelle@users.noreply.github.com>
2024-06-14 18:41:42 -0400

    Trigger the navigation when selected outside of the previewer

--- source/wp-content/themes/wporg-themes-2024/src/theme-patterns/view.js
+++ source/wp-content/themes/wporg-themes-2024/src/theme-patterns/view.js
@@ -1,4 +1,9 @@
 /**
+ * WordPress dependencies
+ */
+import { addQueryArgs } from '@wordpress/url';
+
+/**
  * Internal dependencies
  */
 import wporgListbox from '../utils/listbox';
@@ -12,5 +17,18 @@ window.addEventListener( 'load', () => {
 	containers.forEach( ( container ) => {
 		const state = JSON.parse( container.dataset.initialState );
 		new wporgListbox( container, state );
+
+		// Not in the previewer, use the select event to navigate to the previewer.
+		if ( ! container.closest( '.wp-block-wporg-theme-previewer' ) ) {
+			container.querySelector( '[role="listbox"]' ).addEventListener( 'wporg-select', ( event ) => {
+				const ref = event.selectedElement;
+				if ( ref && ref.dataset ) {
+					let url = window.location.toString();
+					url = url.replace( /\/$/, '' ) + '/preview/';
+					url = addQueryArgs( url, { pattern_name: ref.dataset.pattern_name } );
+					window.location = url;
+				}
+			} );
+		}
 	} );
 } );
